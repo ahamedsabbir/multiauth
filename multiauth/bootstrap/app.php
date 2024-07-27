32b478bf0b8d112108745223951e3a8609840111
Israt Ahamed Sabbir <93650049+ahamedsabbir@users.noreply.github.com>
2024-07-28 00:44:21 +0600

    multiauth setup

--- multiauth/bootstrap/app.php
+++ multiauth/bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\AdminCheck;
+use App\Http\Middleware\AuthorCheck;
+use App\Http\Middleware\EditorCheck;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +14,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias(
+			[
+				'admin' => AdminCheck::class,
+				'author' => AuthorCheck::class,
+				'editor' => EditorCheck::class
+			]
+		);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- multiauth/routes/web.php
+++ multiauth/routes/web.php
@@ -1,7 +1,40 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\EditorController;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+/*Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});*/
+
+
+Route::middleware('admin')->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+});
+
+Route::middleware('author')->group(function () {
+    Route::get('/author/dashboard', [AuthorController::class, 'index'])->name('author.dashboard');
+});
+
+Route::middleware('editor')->group(function () {
+    Route::get('/editor/dashboard', [EditorController::class, 'index'])->name('editor.dashboard');
+});
+
+
+
+require __DIR__.'/auth.php';
